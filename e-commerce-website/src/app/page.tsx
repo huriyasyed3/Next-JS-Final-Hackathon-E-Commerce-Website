1bb477b93762df2c2ac23a433cb6f1fc0577ed59
Huriya Syed <167598780+huriyasyed3@users.noreply.github.com>
2025-01-19 18:01:01 +0500

    update

--- e-commerce-website/src/app/page.tsx
+++ e-commerce-website/src/app/page.tsx
@@ -6,20 +6,21 @@ import PopularProducts from '@/components/Popular-Products'
 import JoinClub from '@/components/join-club'
 import GetInTouch from '@/components/GetInTouch'
 import React from 'react'
-import Data from './data/data'
+// import Data from './data/data'
 const Home = async () => {
     
- const response = await fetch('https://hackathon-apis.vercel.app/api/products')
- const data = await response.json();  
- console.log(data);
+//  const response = await fetch('https://hackathon-apis.vercel.app/api/products')
  
- return (
+//  const data = await response.json();  
+//  console.log(data);
+ 
+  return (
   
    <div>
       
-     {data.map((info: Data) => (
-    <Data info={info} key={info.id} />
-  ))}
+     {/* {data.map((info: Data) => ( }
+     <Data info={info} key={info.id} />
+   ))} */}
       
      
    
--- e-commerce-website/src/sanity/schemaTypes/index.ts
+++ e-commerce-website/src/sanity/schemaTypes/index.ts
@@ -1,7 +1,6 @@
 import {type SchemaTypeDefinition } from 'sanity'
-import { product} from '../schema/product'
-import { Category } from '../schema/category'
+
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [product , Category],
+  types: [],
 }
